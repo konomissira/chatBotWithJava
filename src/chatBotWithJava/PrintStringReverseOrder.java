04378c7f38449217d776173f308941027425efb6
N2SuccessAll <camaram9@lsbu.ac.uk>
2022-12-28 23:22:25 +0000

    Reverse order using scanner!!

---
+++ src/chatBotWithJava/PrintStringReverseOrder.java
@@ -0,0 +1,16 @@
+package chatBotWithJava;
+
+import java.util.Scanner;
+
+public class PrintStringReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String Java = scanner.next();
+        String Programming = scanner.next();
+        String Language = scanner.next();
+
+        System.out.println(Language);
+        System.out.println(Programming);
+        System.out.println(Java);
+    }
+}
