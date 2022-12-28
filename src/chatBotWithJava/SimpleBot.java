ccfe378218000c3d5199d74f6cded78dda7b4ba0
N2SuccessAll <camaram9@lsbu.ac.uk>
2022-12-28 03:02:49 +0000

    Project set up.First commit

---
+++ src/chatBotWithJava/SimpleBot.java
@@ -0,0 +1,8 @@
+package chatBotWithJava;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Ramses.");
+        System.out.println("I was created in 2022.");
+    }
+}
