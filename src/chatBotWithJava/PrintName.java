cbbcca537e8228725c31fd010272f57eef8325a4
N2SuccessAll <camaram9@lsbu.ac.uk>
2022-12-30 18:56:23 +0000

    Increment and decrement in Java in practice: Read four numbers from input then decrementing them and print them.

--- src/chatBotWithJava/PrintName.java
+++ src/chatBotWithJava/PrintName.java
@@ -8,6 +8,7 @@ public class PrintName {
         System.out.println("hello! My name is Aid.");
         System.out.println("I was created in 2022.");
         System.out.println("Please, remind me your name.");
+        System.out.println(24 / 2);
         String name = scanner.next();
         System.out.println("What a great name you have, " + name + "!");
     }
